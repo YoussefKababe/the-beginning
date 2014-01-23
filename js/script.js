e538c562dd86ec1baca872a47265e5a3a40afb40
YoussefKababe <youssef.kbe@gmail.com>
2014-01-23 18:16:26 +0000

    Responsivity update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 $(window).resize(function () { 
-  if (window.innerWidth <= 768) {
-    $(".home").find(".navbar").css("visibility", "visible");
+  if (window.innerWidth <= 992) {
+    $(".home").find(".navbar").css("display", "block");
   } else {
     if ( $(window).scrollTop() >= 100 ) {
-      $(".home").find(".navbar").css("visibility", "visible");
+      $(".home").find(".navbar").css("display", "block");
     } else {
-      $(".home").find(".navbar").css("visibility", "hidden");
+      $(".home").find(".navbar").css("display", "none");
     }
   }
 });
 
-$(".navbar-pills").waypoint(function(direction) {
-  if (window.innerWidth > 768) {
+$(".about").waypoint(function(direction) {
+  if (window.innerWidth > 992) {
     if(direction == "down") {
-      $(".home").find(".navbar").css("visibility", "visible");
+      // $(".home").find(".navbar").css("visibility", "visible");
+      $(".home").find(".navbar").fadeToggle();
     } else {
-      $(".home").find(".navbar").css("visibility", "hidden");
+      $(".home").find(".navbar").fadeToggle();
     }
   }
 });
